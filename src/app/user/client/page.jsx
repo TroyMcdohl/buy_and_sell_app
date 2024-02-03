98865435cbd5b7fb6f0c76b307b42b0278e8906d
troymcdohl <troymcdohl@gmail.com>
2024-02-03 14:15:15 +0630

    a

--- src/app/user/client/page.jsx
+++ src/app/user/client/page.jsx
@@ -1,7 +1,17 @@
 "use client";
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const page = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get("https://buy-and-sell-app-api.vercel.app/api/v1/users", {
+        withCredentials: true,
+      });
+    };
+    fetchData();
+  }, []);
+
   return <div>page</div>;
 };
 
